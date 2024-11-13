a715f1e97fade89b228ff643ddfeaa6f9bc4867c
nunezdelgadomarcos <nunezdelgadomarcos@gmail.com>
2024-11-13 13:22:09 +0000

    Pending changes exported from your codespace

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
+import { name } from "file-loader";
 import "../style/index.css";
+import { left, right } from "@popperjs/core";
 
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
@@ -29,20 +31,58 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  const showFullName = variables.name && variables.lastName;
+  const fullName = `${variables.name} ${variables.lastName}`;
+  const namePlaceHolder = "Input yuor name";
+
+  const workNull = variables.role === null;
+  const showWork = `${variables.role}`;
+  const workPlaceHolder = "Choose your Job";
+
+  const leavingPlace = variables.city && variables.country;
+  const showLeavingPlace = `${variables.city} ${variables.country}`;
+  const leavingPlacePlaceHolder = "Choose your city and country";
+
+  const socialPosition =
+    variables.socialMediaPosition === "position-right"
+      ? "position-right"
+      : "position-left";
+
+  const twitter =
+    variables.twitter === null
+      ? "https://twitter.com/4geeksacademy"
+      : `${variables.twitter}`;
+
+  const gitHub =
+    variables.gitHub === null
+      ? "https://github.com/4geeksacademy"
+      : `${variables.twitter}`;
+
+  const linkedin =
+    variables.linkedin === null
+      ? "https://linkedin.com/school/4geeksacademy"
+      : `${variables.twitter}`;
+
+  const instagram =
+    variables.instagram === null
+      ? "https://instagram.com/4geeksacademy"
+      : `${variables.twitter}`;
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${showFullName ? fullName : namePlaceHolder}</h1>
+          <h2>${workNull ? workPlaceHolder : showWork}</h2>
+          <h3>${leavingPlace ? showLeavingPlace : leavingPlacePlaceHolder}</h3>
+
+          <ul class=${socialPosition}>
+            <li><a href=${twitter}><i class="fab fa-twitter"></i></a></li>
+            <li><a href=${gitHub}><i class="fab fa-github"></i></a></li>
+            <li><a href=${linkedin}><i class="fab fa-linkedin"></i></a></li>
+            <li><a href=${instagram}><i class="fab fa-instagram"></i></a></li>
           </ul>
-        </div>
+      </div>
     `;
 }
 
